f43c69befc854199498ea9f499ab7275e4c10b7a
lskmn <lskmn@outlook.com>
2017-12-19 11:41:35 +0100

    Updated color-picker

--- site/fields/color/color.php
+++ site/fields/color/color.php
@@ -1,13 +1,21 @@
 <?php
+/*
+ * Color Picker Field for Kirby CMS
+ *
+ * Copyright: Ian Cox
+ *
+ * @license: http://opensource.org/licenses/MIT
+ *
+ */
 
 class ColorField extends InputField{
 
   static public $assets = array(
     'js' => array(
-      'minicolors.js',
+      'color.js'
     ),
     'css' => array(
-      'minicolors.css',
+      'minicolors.css'
     )
   );
 
@@ -16,9 +24,10 @@ class ColorField extends InputField{
     $this->icon        = 'paint-brush';
   }
 
-  public function input() {    
+  public function input() {
     $color = new Brick('input');
-    $color->addClass('colorpicker');
+    $color->addClass('input colorpicker');
+    $color->data('field', 'colorfield');
 
     if($this->value() == "" && $this->default() !== ""):
       $value = $this->default();
@@ -31,12 +40,45 @@ class ColorField extends InputField{
     $color->attr(array(
       'name'         => $this->name(),
       'id'           => $this->id(),
-      'type'           =>"text",
+      'disabled'     => $this->disabled(),
+      'readonly'     => $this->readonly(),
+      'type'         => "text",
       'data-defaultvalue' => $value,
       'value'    => $value
     ));
 
-    $color->append($this->option('', '', $this->value() == ''));   
+    // implement OPACITY option
+    // if opacity is used, we must use RGB format
+    if($this->opacity() == true):
+      $color->attr(array(
+        'data-format' => 'rgb',
+        'data-opacity' => '1'
+      ));
+    else:
+      $color->attr(array(
+        'data-format' => 'hex'
+      ));
+    endif;
+
+    // implement CONTROL option
+    if($this->control() == "brightness" || $this->control() == "saturation" || $this->control() == "wheel"):
+      $color->attr(array(
+        'data-control' => $this->control()
+      ));
+    else:
+      $color->attr(array(
+        'data-control' => 'hue'
+      ));
+    endif;
+
+    // implement SWATCHES option
+    if($this->swatches() && $this->swatches() !== ""){
+      $color->attr(array(
+        'data-swatches' => implode('|', $this->swatches())
+      ));
+    }
+
+    $color->append($this->option('', '', $this->value() == ''));
 
     $wrapper = new Brick('div');
     $wrapper->addClass('input color-wrapper');
@@ -44,5 +86,5 @@ class ColorField extends InputField{
 
     return $color;
   }
-  
+
 }
